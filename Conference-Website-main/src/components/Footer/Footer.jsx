239ff0b84bcf0abfe97519343734555c4293a4d3
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-08-18 18:34:57 +0530

    Update Footer.jsx

--- Conference-Website-main/src/components/Footer/Footer.jsx
+++ Conference-Website-main/src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+import React from "react";
 import {
   FaFacebook,
   FaInstagram,
   FaLinkedin,
+  FaTwitter,
 } from "react-icons/fa";
 
 export const Navlinks = [
@@ -44,23 +46,15 @@ const Footer = () => {
                 <a href="#">
                   <FaLinkedin className="text-3xl hover:text-primary duration-300" />
                 </a>
+                <a href="#">
+                  <FaTwitter className="text-3xl hover:text-primary duration-300" />
+                </a>
               </div>
             </div>
           </div>
           <div className="sm:block hidden">Contact us: +91 123456789</div>
           <nav className="hidden md:block">
-            <ul className="flex items-center gap-8">
-              {Navlinks.map(({ id, name, link }) => (
-                <li key={id} className="py-4">
-                  <a
-                    href={link}
-                    className="inline-block text-lg font-semibold  hover:text-primary py-1 hover:border-primary transition-colors duration-300  "
-                  >
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
+           
           </nav>
         </div>
       </div>
