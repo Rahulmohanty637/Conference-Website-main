e65ee425c5e57ca1a907a29320e8dd4e04fc8f6f
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-08-21 14:45:39 +0530

    Update OrganizingCommittee.jsx

--- Conference-Website-main/src/components/OrganizingCommittee/OrganizingCommittee.jsx
+++ Conference-Website-main/src/components/OrganizingCommittee/OrganizingCommittee.jsx
@@ -16,7 +16,7 @@ const OrganizingChair = () => {
         Chief Patron
       </h1>
       <div data-aos="fade-up" className="flex pt-8 items-center justify-center">
-        <div className="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-2">
           <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
             <div className="h-96 w-72">
               <img
@@ -59,6 +59,16 @@ const OrganizingChair = () => {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+      <h1
+        data-aos="fade-up"
+        className="pt-8 mt-16 text-3xl text-black neuton-regular font-bold text-center sm:text-4xl "
+      >
+        Patron
+      </h1>
+      <div data-aos="fade-up" className="flex pt-8 items-center justify-center">
+        <div className="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-2">
           <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
             <div className="h-96 w-72">
               <img
@@ -74,6 +84,27 @@ const OrganizingChair = () => {
               </h1>
               <h3 className="font-dmserif text-md heading font-bold text-white">
                 Vice-Chancellor
+              </h3>
+              <p className="mb-3 text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Centurion University of Technology and Management, India
+              </p>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-96 w-72">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-1 group-hover:scale-125"
+                src="https://cutm.ac.in/wp-content/uploads/2020/01/arpita-mam.jpg"
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-2">
+              <h1 className="font-dmserif text-lg heading font-bold text-white">
+                Prof. (Dr.) Anita Patra
+              </h1>
+              <h3 className="font-dmserif text-md heading font-bold text-white">
+                Registrar
               </h3>
               <p className="mb-3 text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                 Centurion University of Technology and Management, India
