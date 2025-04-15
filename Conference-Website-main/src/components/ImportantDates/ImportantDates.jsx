e7749808146d0a5ee7b4497c79ea7fdee3f30ce7
saneevdas123 <99493297+saneevdas123@users.noreply.github.com>
2025-04-15 10:17:37 +0530

    Update ImportantDates.jsx

--- Conference-Website-main/src/components/ImportantDates/ImportantDates.jsx
+++ Conference-Website-main/src/components/ImportantDates/ImportantDates.jsx
@@ -15,7 +15,7 @@ const ImportantDates = () => {
         <AiOutlineFileDone className="max-w-[200px] w-16 h-16 mx-auto hover:scale-110 duration-300 img-shadow " />
       ),
       title: "Final Submission of manuscripts for review (Full Paper)",
-      date: "20th April, 2025",
+      date: "20th May, 2025",
     },
     {
       id: 2,
